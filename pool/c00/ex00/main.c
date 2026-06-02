8ef66b9ab53f1de84f0ebc24f53d1ee946746b02
PabloMonteiroX <pablo_monteiro@hotmail.com>
2026-06-02 16:09:50 +0200

    c00: add main.c test files to ex00-ex03

---
+++ pool/c00/ex00/main.c
@@ -0,0 +1,15 @@
+/*
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+int	main(void)
+{
+	ft_putchar('H');
+	ft_putchar('o');
+	ft_putchar('l');
+	ft_putchar('a');
+	ft_putchar('\n');
+	return (0);
+}
+*/
