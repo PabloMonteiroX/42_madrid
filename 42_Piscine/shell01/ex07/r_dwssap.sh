6573bc3e78a1c2e8d27d39fedb6ede20c3dbd7f4
Pablo Monteiro <pablo_monteiro@hotmail.com>
2026-06-02 12:18:17 +0200

    Delete 42_Piscine directory

--- 42_Piscine/shell01/ex07/r_dwssap.sh
+++
@@ -1,1 +0,0 @@
-grep -v '^#' /etc/passwd | awk 'NR % 2 == 0' | cut -d: -f1 | sed -n "${FT_LINE1},${FT_LINE2}p" | rev | sort -r | paste -sd ", " - | sed 's/$/./'
--- 42_Piscine/shell01/ex08/add_chelou.sh
+++
@@ -1,1 +0,0 @@
-echo "obase=12; ibase=5; $(echo $FT_NBR1 | tr \"\\'\"\"?!\" 01234) + $(echo $FT_NBR2 | tr mrdoc 01234)" | bc | tr 0123456789AB gtaio\ luSnemf
