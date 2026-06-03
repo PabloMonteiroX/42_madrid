356a7daaf2c299380c7e651b17598c19fb145e2f
PabloMonteiroX <pablo_monteiro@hotmail.com>
2026-06-03 12:59:27 +0200

    c00/ex08: ft_print_combn

--- pool/c00/ex08/main.c
+++ pool/c00/ex08/main.c
@@ -1,17 +1,10 @@
 /*
-#include <unistd.h>
-
 void	ft_print_combn(int n);
 
 int	main(void)
 {
-	char	nline;
-
-	nline = '\n';
 	ft_print_combn(2);
-	write(1, &nline, 1);
 	ft_print_combn(3);
-	write(1, &nline, 1);
 	return (0);
 }
 */
