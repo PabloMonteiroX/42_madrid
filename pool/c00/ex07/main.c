65510f097a97715da7ba6c456afd0eb740030adc
PabloMonteiroX <pablo_monteiro@hotmail.com>
2026-06-03 09:05:41 +0200

    c00/ex07: ft_putnbr

---
+++ pool/c00/ex07/main.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+
+void	ft_putnbr(int nb);
+
+int	main(void)
+{
+	ft_putnbr(0);
+	write(1, "\n", 1);
+
+	ft_putnbr(7);
+	write(1, "\n", 1);
+
+	ft_putnbr(42);
+	write(1, "\n", 1);
+
+	ft_putnbr(-42);
+	write(1, "\n", 1);
+
+	ft_putnbr(12345);
+	write(1, "\n", 1);
+
+	ft_putnbr(-2147483648);
+	write(1, "\n", 1);
+
+	return (0);
+}
+
