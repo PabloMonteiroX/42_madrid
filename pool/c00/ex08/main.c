177d237ac7d00c6a7d0f282c77d65be26a637fa2
PabloMonteiroX <pablo_monteiro@hotmail.com>
2026-06-08 12:30:17 +0200

    c00: limpieza, elimina gitkeep y test

--- pool/c00/ex08/main.c
+++ pool/c00/ex08/main.c
@@ -1,10 +1,17 @@
-/*
+#include <unistd.h>
+
 void	ft_print_combn(int n);
 
 int	main(void)
 {
+    ft_print_combn(1);
+    write(1, "\n", 1);
 	ft_print_combn(2);
+    write(1, "\n", 1);
 	ft_print_combn(3);
+    write(1, "\n", 1);
+    ft_print_combn(4);
+    write(1, "\n", 1);
 	return (0);
 }
-*/
+
