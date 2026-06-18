05e21fb32a48c17ac003a44c08d83d0db1a7473a
PabloMonteiroX <pablo_monteiro@hotmail.com>
2026-06-18 11:07:22 +0200

    c04/ex03: ft_atoi

--- pool/c04/ex03/ft_atoi.c
+++ pool/c04/ex03/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: pmonteir <pablo_monteiro@hotmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/18 09:56:44 by pmonteir          #+#    #+#             */
-/*   Updated: 2026/06/18 10:17:52 by pmonteir         ###   ########.fr       */
+/*   Updated: 2026/06/18 11:06:47 by pmonteir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,20 @@ int	ft_atoi(char *str)
 	int	sign;
 	int	res;
 
-    sign = 1;
-    res = 0;
-    while (*str == ' ' || (*str >= 9 && *str <= 13))
-        str++;
-    while (*str == '+' || *str == '-')
-    {
-        if (*str == '-')
-            sign *= -1;
-        str++;
-    }
-    while (*str >= '0' && *str <= '9')
-    {
-        res = res * 10 + (*str - '0');
-        str++;
-    }
-    return (res * sign);
+	sign = 1;
+	res = 0;
+	while (*str == ' ' || (*str >= 9 && *str <= 13))
+		str++;
+	while (*str == '+' || *str == '-')
+	{
+		if (*str == '-')
+			sign *= -1;
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		res = res * 10 + (*str - '0');
+		str++;
+	}
+	return (res * sign);
 }
